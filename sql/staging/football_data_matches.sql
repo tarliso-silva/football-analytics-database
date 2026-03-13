00442475b23b1e08f5e3ef5f25c85e3de3969f16
Tarliso Doria <tarliso.doria@gmail.com>
2026-03-13 18:28:53 -0300

    Update staging table to match full football-data dataset schema

--- sql/staging/football_data_matches.sql
+++ sql/staging/football_data_matches.sql
@@ -1,12 +1,37 @@
 CREATE TABLE staging.football_data_matches (
-    country TEXT,
-    league TEXT,
-    season TEXT,
-    date TEXT,
-    time TEXT,
-    home TEXT,
-    away TEXT,
-    hg INTEGER,
-    ag INTEGER,
-    res TEXT
+
+country TEXT,
+league TEXT,
+season TEXT,
+
+date TEXT,
+time TEXT,
+
+home TEXT,
+away TEXT,
+
+hg INTEGER,
+ag INTEGER,
+res TEXT,
+
+psch NUMERIC,
+pscd NUMERIC,
+psca NUMERIC,
+
+maxch NUMERIC,
+maxcd NUMERIC,
+maxca NUMERIC,
+
+avgch NUMERIC,
+avgcd NUMERIC,
+avgca NUMERIC,
+
+bfech NUMERIC,
+bfecd NUMERIC,
+bfeca NUMERIC,
+
+b365ch NUMERIC,
+b365cd NUMERIC,
+b365ca NUMERIC
+
 );
