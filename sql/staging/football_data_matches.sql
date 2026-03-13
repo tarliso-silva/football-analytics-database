b43ce3300d7db7028b8ccc347157458dd1e50a68
Tarliso Doria <tarliso.doria@gmail.com>
2026-03-13 16:54:44 -0300

    Create staging table football_data_matches

---
+++ sql/staging/football_data_matches.sql
@@ -0,0 +1,12 @@
+CREATE TABLE staging.football_data_matches (
+    country TEXT,
+    league TEXT,
+    season TEXT,
+    date TEXT,
+    time TEXT,
+    home TEXT,
+    away TEXT,
+    hg INTEGER,
+    ag INTEGER,
+    res TEXT
+);
