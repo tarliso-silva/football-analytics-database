ec773f682c55e49f225fdaef232f71a1a174b285
Tarliso Doria <tarliso.doria@gmail.com>
2026-03-14 14:40:55 -0300

    Add competition dimension load to transformation script

--- sql/transform/load_dimensions.sql
+++ sql/transform/load_dimensions.sql
@@ -6,3 +6,20 @@ INSERT INTO core.country (country_name)
 
 SELECT DISTINCT country
 FROM staging.football_data_matches;
+
+
+
+-- ---------------------------------------------------------
+-- LOAD COMPETITION DIMENSION
+-- ---------------------------------------------------------
+
+INSERT INTO core.competition (competition_name, country_id)
+
+SELECT DISTINCT
+    s.league,
+    c.country_id
+
+FROM staging.football_data_matches s
+
+JOIN core.country c
+    ON c.country_name = s.country;
